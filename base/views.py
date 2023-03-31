073a4aa95be51ede0064930514e6db3a599153d2
Michael Mesfin <michael.21duke@gmail.com>
2023-03-31 14:32:00 +0300

    Render room list in homepage

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+  { 'id': 1, 'name': 'Let\'s learn Django RESTful API' },
+  { 'id': 2, 'name': 'Let\'s learn React' },
+  { 'id': 3, 'name': 'React Native Developers Assemble' },
+]
 def home(request):
-    return render(request, 'home.html')
+    context = { 'rooms': rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
