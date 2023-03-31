9cf1bee0f7ce266e319254012debf9245a855427
Michael Mesfin <michael.21duke@gmail.com>
2023-03-31 19:23:36 +0300

    Remove rooms list and use Room Model

--- base/views.py
+++ base/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-  { 'id': 1, 'name': 'Let\'s learn Django RESTful API' },
-  { 'id': 2, 'name': 'Let\'s learn React' },
-  { 'id': 3, 'name': 'React Native Developers Assemble' },
-]
+# rooms = [
+#   { 'id': 1, 'name': 'Let\'s learn Django RESTful API' },
+#   { 'id': 2, 'name': 'Let\'s learn React' },
+#   { 'id': 3, 'name': 'React Native Developers Assemble' },
+# ]
+
 def home(request):
+    rooms = Room.objects.all().order_by('id')
     context = { 'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, roomid):
-    room = None
-    for r in rooms:
-        if r['id'] == roomid:
-            room = r
-            break
+    try:
+        room = Room.objects.get(id=roomid)
+    except Room.DoesNotExist:
+        room = None
+        
     context = { 'room': room}
     errormsg = { 'error': 'Room not found', 'room': room }
     if room is None:
