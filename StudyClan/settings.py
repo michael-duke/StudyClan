8b17f23b44d07e34b4138318464163f9fe6bfe6f
Michael Mesfin <michael.21duke@gmail.com>
2023-03-31 15:20:01 +0300

    Remove security key from settings.py

--- StudyClan/settings.py
+++ StudyClan/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_u_4s+1$o22%4iotb8f+wz4oif*s3u&e=jl-iv7sm9^m5n7b_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
