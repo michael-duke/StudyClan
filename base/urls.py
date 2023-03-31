7007474d2e686a6340cde1c13226621ab23c634d
Michael Mesfin <michael.21duke@gmail.com>
2023-03-31 22:11:15 +0300

    Implementing the delete view, create view and update view for the room model. Also, adding the urls for the views.

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<int:roomid>', views.room, name='room'),
+    path('create-room', views.createRoom, name='create-room'),
+    path('update-room/<int:roomid>', views.updateRoom, name='update-room'),
+    path('delete-room/<int:roomid>', views.deleteRoom, name='delete-room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 # rooms = [
@@ -9,7 +10,7 @@ from .models import Room
 # ]
 
 def home(request):
-    rooms = Room.objects.all().order_by('id')
+    rooms = Room.objects.all()
     context = { 'rooms': rooms}
     return render(request, 'base/home.html', context)
 
@@ -25,3 +26,37 @@ def room(request, roomid):
         return render(request, 'base/room.html', errormsg)
     else:
         return render(request, 'base/room.html', context)
+    
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, roomid):
+    room = Room.objects.get(id=roomid)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, roomid):
+    room = Room.objects.get(id=roomid)
+    
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+        
+    context = {'item': room}
+    return render(request, 'base/delete.html', context)
