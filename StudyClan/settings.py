a049963564fa8084642a7d9616141c712732c35e
Michael Mesfin <michael.21duke@gmail.com>
2023-04-06 20:31:00 +0300

    Add in the CORS_ALLOWED_ORIGINS

--- StudyClan/settings.py
+++ StudyClan/settings.py
@@ -63,6 +63,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'StudyClan.urls'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
