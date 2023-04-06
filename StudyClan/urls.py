30d630f357b6202f219c123b102a2dc77aaac4c6
Michael Mesfin <michael.21duke@gmail.com>
2023-04-06 20:18:14 +0300

    Implement routing for api/v1 namespace in core urlspatterns

--- StudyClan/urls.py
+++ StudyClan/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('api/v1/', include('api.urls')),
     path('__reload__/', include('django_browser_reload.urls')),
 ]
